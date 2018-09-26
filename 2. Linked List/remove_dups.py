3ffea5b5f82b2ade2044640ba4ede71f7fd93379
chejain <chetan0018@gmail.com>
2018-09-26 22:21:31 +0530

    Including Remove Duplicate function

--- 2. Linked List/remove_dups.py
+++ 2. Linked List/remove_dups.py
@@ -25,16 +25,36 @@ class LinkedList:
         print ('')
         return
 
+    def removeDuplicates(self):
+        if self.head is None or self.head.next is None:
+            return
+
+        currPtr = self.head
+        while currPtr:
+            nextPtr = currPtr.next
+            prevPtr = currPtr
+            while nextPtr:
+                if currPtr.data == nextPtr.data:
+                    prevPtr.next = nextPtr.next
+                else:
+                    prevPtr = nextPtr
+                nextPtr = nextPtr.next
+            currPtr = currPtr.next
+        return
+
+
 def main():
     head = LinkedList()
-    head.push(2)
     head.push(3)
     head.push(3)
+    # head.push(2)
     head.display()
-    head.push(2)
+    # head.push(3)
 
     head.display()
-
+    head.removeDuplicates()
+    print ('after duplicates')
+    head.display()
 
 if __name__ == '__main__':
     main()
